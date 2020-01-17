df59595b0bfb8f70fcba4ccfbc44d5683baf300d
nianze <nianzelee@gmail.com>
2020-01-17 18:02:54 +0100

    dnn_mnist exp dir done

--- exp_TCAD/dnn_mnist/script/exp_dnn.py
+++ exp_TCAD/dnn_mnist/script/exp_dnn.py
@@ -1,5 +1,5 @@
 '''*************************************************************
-FileName    [exp.py]
+FileName    [exp_dnn.py]
 ProjectName [Threshold logic synthesis and verification.]
 Synopsis    [Script for DNN experiment.]
 Author      [Nian-Ze Lee]
@@ -13,13 +13,13 @@ a = [16, 32, 48, 64, 128]
 b = [3, 4, 5]
 for n_input in a:
    for n_layer in b:
-      N = 'exp/dnn_mnist/dnn/nianze_exp_%d_%d_256.th' % (n_input, n_layer)
-      M = 'exp/dnn_mnist/dnn/nianze_exp_%d_%d_16.th' % (n_input, n_layer)
-      f = 'exp/dnn_mnist/dnn_opb/%d_%d.opb' % (n_input, n_layer)
-      g = 'exp/dnn_mnist/dnn_log/%d_%d.log' % (n_input, n_layer)
+      N = 'exp_TCAD/dnn_mnist/benchmark/binarized_dnn_%d_%d_256.th' % (n_input, n_layer)
+      M = 'exp_TCAD/dnn_mnist/benchmark/binarized_dnn_%d_%d_16.th' % (n_input, n_layer)
+      f = 'exp_TCAD/dnn_mnist/opb/%d_%d.opb' % (n_input, n_layer)
+      g = 'exp_TCAD/dnn_mnist/log/%d_%d.log' % (n_input, n_layer)
       cmd = './bin/abc -c \"thverify ' + N + ' ' + M + '\"'
       sp.run(cmd, shell=True)
       cmd = 'mv compTH.opb ' + f
       sp.run(cmd, shell=True)
-      cmd = './bin/minisat+ ' + f + ' &> ' + g + ' &'
+      cmd = './bin/minisat+ ' + f + ' > ' + g + ' &'
       sp.run(cmd, shell=True)
